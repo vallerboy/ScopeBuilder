dedf965ce83e7b7bc3fec9383d1da14bdc889130
Oskar Polak <oskarpolak@vp.pl>
2017-11-16 18:42:37 +0100

    eee

--- src/main/java/pl/oskarpolak/springsec/models/services/DistanceService.java
+++ src/main/java/pl/oskarpolak/springsec/models/services/DistanceService.java
@@ -1,8 +1,17 @@
 package pl.oskarpolak.springsec.models.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import pl.oskarpolak.springsec.models.DistanceModel;
 
 @Service
 public class DistanceService {
+
+    @Value("${google.distancematrix.key}")
+    String googleKey;
+
+    public DistanceModel getDistance(String from, String to){
+        String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + from + "&destinations=" + to + "&key=" + googleKey;
+    }
 
 }
